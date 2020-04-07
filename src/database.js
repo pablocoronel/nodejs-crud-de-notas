d4e2809355e488b360e7c883a78007adfc7f266d
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-04-07 00:20:35 -0300

    config for connection to database

---
+++ src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// conexion a la bd
+// const NOTES_APP_MONGODB_HOST = process.env.NOTES_APP_MONGODB_HOST;
+// const NOTES_APP_MONGODB_DATABASE = process.env.NOTES_APP_MONGODB_DATABASE;
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env; // en .env
+
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+
+// configuracion de mongoose
+mongoose
+	.connect(MONGODB_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then((db) => console.log('Database is connected'))
+	.catch((err) => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+require('dotenv').config(); // carga el archivo .env
+
 const app = require('./server');
+require('./database');
+
+// process.env accede a las variables del sistema
+console.log(process.env.TESTING);
 
 app.listen(app.get('port'), () => {
 	console.log('server on port', app.get('port'));
