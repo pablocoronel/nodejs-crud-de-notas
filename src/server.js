4129ca0c83d799f665a2a277557e7e7ccde1ea4b
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-04-07 21:22:06 -0300

    configuration of handlebars, created folders layouts and partials

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path'); //nodejs
+const expressHandlebars = require('express-handlebars');
 
 // Initials
 const app = express();
@@ -7,6 +8,16 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views')); // para que express sepa la ruta (multi os) de la carpeta
+app.engine(
+	'.hbs',
+	expressHandlebars({
+		defaultLayout: 'main',
+		layoutsDir: path.join(app.get('views'), 'layouts'), // usa el set views
+		partialsDir: path.join(app.get('views'), 'partials'), // usa el set views
+		extname: '.hbs',
+	})
+);
+app.set('view engine', '.hbs'); //usa el engine de arriba
 
 // Middlewares
 app.set(express.urlencoded({ extended: false })); // los datos del formulario se convierten a JSON
@@ -16,7 +27,8 @@ app.set(express.urlencoded({ extended: false })); // los datos del formulario se
 // Routes
 // ruta basica
 app.get('/', (req, res) => {
-	res.send('hello world');
+	// res.send('hello world');
+	res.render('index');
 });
 
 // Static files
