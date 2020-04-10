1a2d6a7e630fb9b9125de800c761457813eae529
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-04-09 21:44:47 -0300

    list notes in view, added html code for edit and delete Note

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,19 +1,29 @@
 const notesController = {};
 
+// Modelo
+const Note = require('../models/Note');
+
 // Crear
 notesController.renderNoteForm = (req, res) => {
 	res.render('models/notes/new-note');
 };
 
-notesController.createNewNote = (req, res) => {
-	console.table(req.body);
+notesController.createNewNote = async (req, res) => {
+	const { title, description } = req.body;
+
+	// Guardar en bd
+	const newNote = new Note({ title, description });
+	await newNote.save(); // se guarda en la bd, es una operacion asincrona
 
 	res.send('new note');
 };
 
 // Listar
-notesController.renderNotes = (req, res) => {
-	res.send('list');
+notesController.renderNotes = async (req, res) => {
+	// Busca en el modelo
+	const notes = await Note.find().lean(); // es asincrono
+
+	res.render('models/notes/all-notes', { notes }); // la data se pasa en un objeto
 };
 
 // Editar
