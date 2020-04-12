93aa429bf9254298b185e24c6bb52e7e3435aa53
pablo Coronel <pablomanuelcoronel@gmail.com>
2020-04-11 22:16:44 -0300

    messages between views with sessions

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -15,6 +15,7 @@ notesController.createNewNote = async (req, res) => {
 	const newNote = new Note({ title, description });
 	await newNote.save(); // se guarda en la bd, es una operacion asincrona
 
+	req.flash('success_msg', 'Note added successfully'); // guarda el mensaje en el servidor
 	res.redirect('/notes');
 };
 
@@ -36,12 +37,15 @@ notesController.updateNote = async (req, res) => {
 	const { title, description } = req.body;
 	await Note.findByIdAndUpdate(req.params.id, { title, description });
 
+	req.flash('success_msg', 'Note updated successfully'); // guarda el mensaje en el servidor
 	res.redirect('/notes');
 };
 
 // Borrar
 notesController.deleteNote = async (req, res) => {
 	await Note.findByIdAndDelete(req.params.id); // busca por id y lo borra de la bd
+
+	req.flash('success_msg', 'Note deleted successfully'); // guarda el mensaje en el servidor
 	res.redirect('/notes'); // redirecciona a la vista de listar
 };
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const path = require('path'); //nodejs
-const expressHandlebars = require('express-handlebars');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
+const express = require('express'); // para server
+const path = require('path'); //nodejs: rutas
+const expressHandlebars = require('express-handlebars'); // para plantillas de vista
+const morgan = require('morgan'); // para loger
+const methodOverride = require('method-override'); // para usar PUT y DELETE en http
+const flash = require('connect-flash'); // para enviar mensajes
+const session = require('express-session'); // para guardar los mensajes enviados entre vistas
 
 // Initials
 const app = express();
@@ -25,8 +27,23 @@ app.set('view engine', '.hbs'); //usa el engine de arriba
 app.use(morgan('dev')); // sirve para log de la app en consola
 app.use(express.urlencoded({ extended: false })); // los datos del formulario en el request se convierten a JSON
 app.use(methodOverride('_method')); // Para usar el verbo Delete en http
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true,
+	})
+); // configuracion de express-session
+app.use(flash());
 
-// Global variables
+// Global variables (se ven en toda la aplicacion)
+// usar un middleware propio
+app.use((req, res, next) => {
+	// locals guarda lo almacenado en flash, en las variables del servidor
+	res.locals.success_msg = req.flash('success_msg');
+
+	next();
+});
 
 // Routes
 app.use(require('./routes/index.routes'));
